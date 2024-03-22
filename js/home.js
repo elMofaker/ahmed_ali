0803dfc77fe49870077fef6ec756ac03cee7a24e
elMofaker <155926314+elMofaker@users.noreply.github.com>
2024-03-22 17:20:32 +0200

    q

--- js/home.js
+++ js/home.js
@@ -1,22 +1,19 @@
-// active link nav
-let div=document.querySelectorAll('div')
-let nav_link=document.querySelectorAll('nav a')
-window.onscroll=()=>{
-    div.forEach(sec=>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop-150;
-        let height=sec.offsetHeight;
-        let id=sec.getAttribute('id')
-        if(top>offset && top<offset + height){
-            nav_link.forEach(links=>{
-                links.classList.remove('active')
-                document.querySelector('nav a[href*='+id+']').classList.add('active')
-            })
-        }
-    })
-}
+// // active link nav
+// let div=document.querySelectorAll('div')
+// let nav_link=document.querySelectorAll('nav a')
+// window.onscroll=()=>{
+//     div.forEach(sec=>{
+//         let top=window.scrollY;
+//         let offset=sec.offsetTop-150;
+//         let height=sec.offsetHeight;
+//         let id=sec.getAttribute('id')
+//         if(top>offset && top<offset + height){
+//             nav_link.forEach(links=>{
+//                 links.classList.remove('active')
+//                 document.querySelector('nav a[href*='+id+']').classList.add('active')
+//             })
+//         }
+//     })
+// }
 
 // scroll nav style 
-
-
-
